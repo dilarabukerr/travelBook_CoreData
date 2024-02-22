64091aa4f89aa4d606890c5b656a5e2aeef14eba
Dilara Büker <72442245+dilarabukerr@users.noreply.github.com>
2024-02-22 15:28:14 +0300

    travelBook

---
+++ travelBook_CoreData/travelBook_CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  travelBook_CoreData
+//
+//  Created by Dilara Büker on 22.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
