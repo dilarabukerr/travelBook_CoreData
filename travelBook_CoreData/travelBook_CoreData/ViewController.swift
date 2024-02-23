92c38a9714b0a22d8d253bd8ef93ceaac7c59979
Dilara Büker <72442245+dilarabukerr@users.noreply.github.com>
2024-02-23 14:05:53 +0300

    travelBook

--- travelBook_CoreData/travelBook_CoreData/ViewController.swift
+++ travelBook_CoreData/travelBook_CoreData/ViewController.swift
@@ -6,16 +6,46 @@
 //
 
 import UIKit
-import MapKit
+import MapKit //harita kullanmak için
+import CoreLocation //kullanıcıdan konum almak için
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManager = CLLocationManager() // Konum yöneticisi tanımı
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.delegate = self 
-
+        
+        //Konum izni isteği
+        locationManager.delegate = self //konum yöneticisinin delegesini ayarla
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest //lokasyonun güvenilirliğini ayarlama.
+        locationManager.requestWhenInUseAuthorization() //kullanım sırasında konum iste
+        locationManager.startUpdatingLocation()//kullanıcının yerini alma.
+//burada info kısmında privacy ayarlarından location için when in use seçeneğini seçebilirsin.
+        
+        //Harita ayarları
+        mapView.delegate = self
+        mapView.showsUserLocation = true //kullanıcı konumunu haritada göster
     }
-
+    
+    //konum izni değişikliklerini kontrol et
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse { //eğer konum izni kullanım sırasında verildiyse
+            locationManager.startUpdatingLocation()//konum güncellemelerini başlat
+        }
+    }
+    
+    //konum güncellemelerini işle
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {return} //en son konumu al, Eğer locations dizisi boş değilse, en son konumu al ve location sabitine ata, Eğer locations dizisi boş ise, fonksiyonu burada erken sonlandır
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)//konumu merkez alarak bir bölge oluşturur
+        mapView.setRegion(region, animated: true)//harita görünümünün bölgesini güncelle
+    }
+    
+    //konum izni reddedildiğinde
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("konum izni hatası")
+    }
 }
 
